e76db7fc09f3d0f56ab52c6a4a60f09bda1e6ee8
Ky5t0nbr <Kbrowntx@outlook.com>
2023-09-24 19:56:16 -0500

    webpage loaded

--- frontend/src/about/about-metropulse/src/App.js
+++ frontend/src/about/about-metropulse/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from "react-bootstrap";
+//import "bootstrap/dist/css/boostrap.min.css";
 
 function App() {
   return (
@@ -7,15 +9,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/Aps</code> and save to reload.
         </p>
+        <Button> Sample Button </Button>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          OK!
         </a>
       </header>
     </div>
