bb4a411998b12c5b35b69b623f0cb3589765b61f
Kamil Kalowski <kamilkalowski@yahoo.com>
2023-10-20 16:46:47 -0500

    unit tests python unittest

--- backend/unit_tests.py
+++ backend/unit_tests.py
@@ -1,119 +1,90 @@
 import unittest
-import app
+from endpoints import app
+
+# These unit tests were influenced by Park-Dex
 
 class Tests(unittest.TestCase):
     def setUp(self):
-        # app.app.config["TESTING"] = True
-        self.client = app.app.test_client()
+        #flask_app.config["TESTING"] = True
+        self.client = app.test_client()
 
-    def testGetAllHosptialPagination(self):
+    def testInvalidRoute(self):
         with self.client:
-            response = self.client.get("")
-            self.assertEqual(response.status_code, )
-            data = response.json[""]
-            self.assertEqual(len(data), )
+            response = self.client.get("/test")
+            self.assertEqual(response.status_code, 404)
+            
 
-    def testGetAllCenterPagination(self):
+    def testGetHospitalPagination(self):
         with self.client:
-            response = self.client.get("")
-            self.assertEqual()
-            data = response.json[""]
-            self.assertEqual(len(data), )
+            response = self.client.get("/hospitals?page=3&per_page=25")
+            self.assertEqual(response.status_code, 200)
+            data = response.json["data"]
+            self.assertEqual(len(data), 25)
 
-    def testGetAllNeighborhoodPagination(self):
+    def testGetCenterPagination(self):
         with self.client:
-            response = self.client.get("")
-            self.assertEqual()
-            data = response.json[""]
-            self.assertEqual(len(data), )           
+            response = self.client.get("/centers?page=1&per_page=25")
+            self.assertEqual(response.status_code, 200)
+            data = response.json["data"]
+            self.assertEqual(len(data), 25)
+
+    def testGetNeighborhoodPagination(self):
+        with self.client:
+            response = self.client.get("/neighborhoods?page=4&per_page=25")
+            self.assertEqual(response.status_code, 200)
+            data = response.json["data"]
+            self.assertEqual(len(data), 25)           
 
     def testGetAllHospitals(self):
         with self.client:
-            response = self.client.get("")
-            self.assertEqual(response.status_code, )
-            data = response.json[""]
-            self.assertEqual(len(data), )
+            response = self.client.get("/hospitals")
+            self.assertEqual(response.status_code, 200)
+            data = response.json["total_size"]
+            self.assertEqual(data, 78)
 
     def testGetAllCenters(self):
         with self.client:
-            response = self.client.get("")
-            self.assertEqual(response.status_code, )
-            data = response.json[""]
-            self.assertEqual(len(data), )
+            response = self.client.get("centers")
+            self.assertEqual(response.status_code, 200)
+            data = response.json["total_size"]
+            self.assertEqual(data, 1000)
 
     def testGetAllNeighborhoods(self):
         with self.client:
-            response = self.client.get("")
-            self.assertEqual(response.status_code, )
-            data = response.json[""]
-            self.assertEqual(len(data), )
+            response = self.client.get("neighborhoods")
+            self.assertEqual(response.status_code, 200)
+            data = response.json["total_size"]
+            self.assertEqual(data, 195)
 
     def testGetHospitalInstance(self):
         with self.client:
-            response = self.client.get("/api/")
-            self.assertEqual(response.status_code, )
-            resp = response.json
-            data = resp[""]
-            self.assertEqual(data[""], "")
-            self.assertEqual(data[""], "")
-            self.assertEqual(len(data[""]), )
+            response = self.client.get("/hospitals/0")
+            self.assertEqual(response.status_code, 200)
+            full_data = response.json
+            data = full_data["data"]
+            self.assertEqual(data["facility_type"], "Child Health Center")
+            self.assertEqual(data["borough"], "Manhattan")
+            self.assertEqual(data["name"], "La Clinica Del Barrio")
 
     def testGetCenterInstance(self):
         with self.client:
-            response = self.client.get("/api/")
-            self.assertEqual(response.status_code, )
-            resp = response.json
-            data = resp[""]
-            self.assertEqual(data[""], "")
-            self.assertEqual(data[""], "")
-            self.assertEqual(len(data[""]), )
+            response = self.client.get("centers/1")
+            self.assertEqual(response.status_code, 200)
+            full_data = response.json
+            data = full_data["data"]
+            self.assertEqual(data["address"], "1368 Linden Boulevard")
+            self.assertEqual(data["borough"], "BROOKLYN")
+            self.assertEqual(len(data["nearby_hospitals"]), 11)
 
     def testGetNeighborhoodInstance(self):
         with self.client:
-            response = self.client.get("/api/")
-            self.assertEqual(response.status_code, )
-            resp = response.json
-            data = resp[""]
-            self.assertEqual(data[""], "")
-            self.assertEqual(data[""], "")
-            self.assertEqual(len(data[""]), )
-
-    def testHospitalsLength(self):
-        with self.client:
-            response = self.client.get("/api")
+            response = self.client.get("neighborhoods/3")
             self.assertEqual(response.status_code, 200)
-            resp = response.json
-            count = resp[""]
-            self.assertEqual(count, )
-
-    def testCentersLength(self):
-        with self.client:
-            response = self.client.get("/api")
-            self.assertEqual(response.status_code, 200)
-            resp = response.json
-            count = resp[""]
-            self.assertEqual(count, )
-
-    def testNeighborhoodLength(self):
-        with self.client:
-            response = self.client.get("/api")
-            self.assertEqual(response.status_code, )
-            resp = response.json
-            count = resp[""]
-            self.assertEqual(count, )
-
-    def testSearch(self):
-        with self.client:
-            response = self.client.get("/api")
-            self.assertEqual(response.status_code, )
-            resp = response.json
-            animals = resp[""][""]
-            states = resp[""][""]
-            parks = resp[""][""]
-            self.assertEqual(type(), list)
-            self.assertEqual(type(), list)
-            self.assertEqual(type(), list)
-
+            full_data = response.json
+            data = full_data["data"]
+            self.assertEqual(data["borough"], "Bronx")
+            self.assertEqual(data["desc"], "Belmont is a village within the town of Amity in Allegany County, New York, United States. Belmont is the county seat of Allegany County.")
+            self.assertEqual(len(data), 11)
 
 if __name__ == "__main__":
     unittest.main()
